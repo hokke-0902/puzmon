f0ea81d1db7d188325f8fd278b9c0e120ea2b17b
shusuke <hokke0902@gmail.com>
2025-03-27 11:58:37 +0900

    second_commit
    do_battle(),エラーメッセージの作成

--- puzmon.py
+++ puzmon.py
@@ -1,20 +1,37 @@
 #インポート
 
 #グローバル変数の宣言
+monster_names=['スライム','ゴブリン','オオコウモリ','ウェアウルフ','ドラゴン']
 
 #関数宣言
 
 def main():
-    player_name=input('プレイヤー名を入力してください')
+    while True:
+        player_name=input('プレイヤー名を入力してください>')
+        if len(player_name) > 0:
+            break
+        print('エラー:プレイヤー名を入力してください')
+
     print('*** Puzzle & Monsters ***')
     kills=go_dungeon(player_name)
-    print('*** GAME CLEARED!! ***')
+    if kills == len(monster_names):
+        print('*** GAME CLEARED!! ***')
+    else:
+        print('*** GAME OVERA ***')
     print(f'倒したモンスター数={kills}')
 
+def do_battle(monster_name):
+    print(f'{monster_name}が現れた!')
+    print(f'{monster_name}を倒した!')
+    return 1
+
 def go_dungeon(player_name):
+    kills =0
     print(f'{player_name}はダンジョンに到達した!')
+    for m in monster_names:
+        kills += do_battle(m)
     print(f'{player_name}はダンジョンを制覇した!')
-    return 5
+    return kills
 
 #メイン関数の呼び出し
 main()
