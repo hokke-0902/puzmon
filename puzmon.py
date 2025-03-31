cd5761f32db917724f1c3d4eb7cd4d22317f2d67
shusuke <hokke0902@gmail.com>
2025-03-31 13:17:06 +0900

    10th_commit

--- puzmon.py
+++ puzmon.py
@@ -1,227 +1,253 @@
-#インポート
+# インポート
 import random
 
-#グローバル変数の宣言
+# グローバル変数の宣言
 ELEMENT_SYMBOLS={
         '火':'$',
+        '風':'@',
+        '土':'#',
         '水':'~',
-        '土':'@',
-        '風':'#',
         '命':'&',
-        '無':' '
-        }
+        '無':' ',
+}
 ELEMENT_COLORS={
         '火':1,
-        '水':6,
-        '土':3,
         '風':2,
+        '土':3,
+        '水':6,
         '命':5,
-        '無':7
-        }
-
+        '無':7,
+}
 gems=[]
 
 IDXS ='ABCDEFGHIJKLMN'
 
-#関数宣言
+# 関数宣言
 
 def main():
-    monster_list=[
-            {'name':'スライム',
-             'hp':100,
-             'max_hp':100,
-             'element':'水',
-             'ap':10,
-             'dp':1},
-            {'name':'ゴブリン',
-             'hp':200,
-             'max_hp':200,
-             'element':'土',
-             'ap':20,
-             'dp':5},
-            {'name':'オオコウモリ',
-             'hp':300,
-             'max_hp':300,
-             'element':'風',
-             'ap':30,
-             'dp':10},
-            {'name':'ウェアウルフ',
-             'hp':400,
-             'max_hp':400,
-             'element':'風',
-             'ap':40,
-             'dp':15},
-            {'name':'ドラゴン',
-             'hp':600,
-             'max_hp':600,
-             'element':'火',
-             'ap':50,
-             'dp':20}
-            ]
     friends=[
-            {'name':'朱雀',
-             'hp':150,
-             'max_hp':150,
-             'element':'火',
-             'ap':25,
-             'dp':10},
-            {'name':'青龍',
-             'hp':150,
-             'max_hp':150,
-             'element':'風',
-             'ap':15,
-             'dp':10},
-            {'name':'白虎',
-             'hp':150,
-             'max_hp':150,
-             'element':'土',
-             'ap':20,
-             'dp':5},
-            {'name':'玄武',
-             'hp':150,
-             'max_hp':150,
-             'element':'水',
-             'ap':20,
-             'dp':15}
-            ]
+            {
+                'name':'朱雀',
+                'hp':150,
+                'max_hp':150,
+                'element':'火',
+                'ap':25,
+                'dp':10
+            },
+            {
+                'name':'青龍',
+                'hp':150,
+                'max_hp':150,
+                'element':'風',
+                'ap':15,
+                'dp':10
+            },
+            {
+                'name':'白虎',
+                'hp':150,
+                'max_hp':150,
+                'element':'土',
+                'ap':20,
+                'dp':5
+            },
+            {
+                'name':'玄武',
+                'hp':150,
+                'max_hp':150,
+                'element':'水',
+                'ap':20,
+                'dp':15
+            },
+
+    ]
+    monster_list=[
+            {
+                'name':'スライム',
+                'hp':100,
+                'max_hp':100,
+                'element':'水',
+                'ap':10,
+                'dp':1
+            },
+            {
+                'name':'ゴブリン',
+                'hp':200,
+                'max_hp':200,
+                'element':'土',
+                'ap':20,
+                'dp':5
+            },
+            {
+                'name':'オオコウモリ',
+                'hp':300,
+                'max_hp':300,
+                'element':'風',
+                'ap':30,
+                'dp':10
+            },
+            {
+                'name':'ウェアウルフ',
+                'hp':400,
+                'max_hp':400,
+                'element':'風',
+                'ap':40,
+                'dp':15
+            },
+            {
+                'name':'ドラゴン',
+                'hp':600,
+                'max_hp':600,
+                'element':'火',
+                'ap':50,
+                'dp':20
+            },
+    ]
     while True:
-        player_name=input('プレイヤー名を入力してください>')
+        player_name=input('プレイヤー名を入力してください>>')
         if len(player_name) > 0:
             break
         print('エラー:プレイヤー名を入力してください')
 
     print('*** Puzzle & Monsters ***')
-    party = orgnize_party(player_name,friends)
-    kills=go_dungeon(party,monster_list)
+    party=organize_party(player_name,friends)
+    kills = go_dungeon(party,monster_list)
     if kills == len(monster_list):
         print('*** GAME CLEARED!! ***')
     else:
-        print('*** GAME OVERA ***')
+        print('*** GAME OVER!! ***')
+
     print(f'倒したモンスター数={kills}')
+
+
+def go_dungeon(party,monster_list):
+    kills = 0
+    print(f'{party['name']}のパーティ(HP={party['hp']})はダンジョンに到着した')
+    show_party(party)
+    for monster in monster_list:
+        kills += do_battle(party,monster)
+        if party['hp'] <= 0:
+            print(f'{party['name']}はダンジョンから逃げ出した')
+            break
+        print(f'{party['name']}はさらに奥に進んだ')
+        print('==================================')
+    else:
+        print(f'{party['name']}はダンジョンに制覇した')
+
+    return kills
 
 def do_battle(party,monster):
     print_monster_name(monster)
     print('が現れた!')
+
     fill_gems()
+    
     while True:
         on_player_turn(party,monster)
         if monster['hp'] <= 0:
             break
         on_enemy_turn(party,monster)
         if party['hp'] <= 0:
-
-            print('パーティーのHPは0になった')
+            print('パーティのHPは0になった')
             return 0
+        
     print_monster_name(monster)
     print('を倒した!')
     return 1
 
 def print_monster_name(monster):
     monster_name=monster['name']
-    symbol=ELEMENT_SYMBOLS[monster['element']]
+    symbol = ELEMENT_SYMBOLS[monster['element']]
     color=ELEMENT_COLORS[monster['element']]
 
     #モンスター名を表示
     print(f'\033[30;4{color}m{symbol}{monster_name}{symbol}\033[0m',end='')
 
-def go_dungeon(party,monster_list):
-    kills =0
-    print(f'{party['name']}のパーティー(HP={party['hp']})はダンジョンに到達した!')
-    show_party(party)
-    for m in monster_list:
-            kills += do_battle(party,m)
-            if party['hp'] <= 0:
-                print(f'{party['name']}はダンジョンから逃げ出した')
-                break
-            print(f'{party['name']}はさらに奥に進んだ')
-            print('======================')
-    if kills == len(monster_list):
-        print(f'{party['name']}はダンジョンを制覇した!')
-    return kills
+def organize_party(player_name,friends):
+    total_hp = 0
+    total_dp = 0
+    for friend in friends:
+        total_hp += friend['hp']
+        total_dp += friend['dp']
 
-def orgnize_party(player_name,friends):
-    total_hp= -40
-    total_dp=0
-    for f in friends:
-        total_hp += f['hp']
-        total_hp += f['dp']
-
-    party ={
+    party = {
             'name':player_name,
             'friends':friends,
             'hp':total_hp,
             'max_hp':total_hp,
             'dp':total_dp / len(friends)
-            }
+    }
     return party
 
 def show_party(party):
-    print('<パーティー編成>----------')
-    for f in party['friends']:
-        print_monster_name(f)
-        print(f' HP= {f['hp']}')
-        print(f' 攻撃= {f['ap']}')
-        print(f' 防御= {f['dp']}')
-    print('--------------------------')
+    print('<パーティ編成>----------------------')
+    for friend in party['friends']:
+        print_monster_name(friend)
+        print(f' HP= {friend['hp']} 攻撃={friend['ap']} 防御= {friend['dp']}')
+    print('------------------------------------')
+    print()
+
 
 def on_player_turn(party,monster):
     print(f'\n【{party['name']}のターン】(HP={party['hp']})')
     show_battle_field(party,monster)
     while True:
-        command=input('コマンド? >').upper()
+        command = input('コマンド? >').upper()
         if check_valid_command(command):
             break
         print('エラー:正しいコマンドを入力してください')
 
     move_gem(command)
-    evalueate_gems(monster,command)
+    evaluate_gems(party,monster)
+
 
 def on_enemy_turn(party,monster):
     print(f'\n【',end='')
     print_monster_name(monster)
     print(f'のターン】(HP={monster['hp']})')
-    do_enemy_attack(party)
+    do_enemy_attack(party,monster)
+def do_attack(friend,monster):
+    basic_damage=friend['ap'] - monster['dp']
+    boost_damage=element_boost(friend,monster)
+    combo = 1
 
-def do_attack(monster,command):
-    damage=hash(command) % 50
-    rand= random.uniform(-0.1,0.1)+1
-    damage = int(damage * rand)
+    damage = max(1,int(basic_damage * boost_damage * combo))
+    dagame = blur_damage(damage)
     print(f'{damage}のダメージを与えた')
     monster['hp'] -= damage
-    
-
-def do_enemy_attack(party):
-    damage=200
+def do_enemy_attack(party,monster):
+    damage = monster['ap'] - party['dp']
+    damage = max(1,blur_damage(damage))
     print(f'{damage}のダメージを受けた')
     party['hp'] -= damage
 
 def show_battle_field(party,monster):
     print('バトルフィールド')
     print_monster_name(monster)
-    print(f'HP = {monster['hp']} / {monster['max_hp']}\n')
-    for f in party['friends']:
-        print_monster_name(f)
+    print(f'HP = {monster['hp']} / {monster['max_hp']}')
+
+    for friend in party['friends']:
+        print_monster_name(friend)
         print(' ',end='')
     print(f'\nHP = {party['hp']} / {party['max_hp']}\n')
-    print('--------------------')
+    print('------------------------------')
     for c in IDXS:
         print(c,end=' ')
     print()
     print_gems()
-    print('--------------------')
+    print('------------------------------')
 
+    
 def fill_gems():
     global gems
-    gems=[random.randint(0,4) for i in range(len(IDXS))]
+    gems=[random.randint(0,4) for _ in range(len(IDXS))]
 
 def print_gems():
-    eles=['火','水','土','風','命','無']
+    eles=['火','水','風','土','命','無']
     for i in gems:
         color=ELEMENT_COLORS[eles[i]]
-        symbol=ELEMENT_SYMBOLS[eles[i]]
+        symbol =ELEMENT_SYMBOLS[eles[i]]
         print(f'\033[30;4{color}m{symbol}\033[0m',end=' ')
     print()
-
 def check_valid_command(command):
     if len(command) != 2:
         return False
@@ -239,29 +265,31 @@ def move_gem(command):
     print_gems()
     print()
     for i in range(b_index,e_index,dir):
-        gems[i],gems[i+dir]=gems[i+dir],gems[i]
+        gems[i],gems[i+dir] = gems[i+dir],gems[i]
         print_gems()
         print()
 
-def evalueate_gems(monster,command):
+def evaluate_gems(party,monster):
     start_idx = check_banishable()
     if start_idx != -1:
-        banish_gems(start_idx)
-        do_attack(monster,command)
+        gem=banish_gems(start_idx)
+        if gem == 4:
+            do_recover(party,1)
+        else:
+            do_attack(party['friends'][gem],monster)
         shift_gems()
     else:
         pass
-    do_attack(monster,command)
 
 def check_banishable():
     count = 1
     for i in range(1,len(gems)):
         if gems[i] == gems[i-1]:
-            count += 1
+            count+=1
             if count == 3:
-                return i -2
+                return i-2
         else:
-            count = 1
+            count=1
     return -1
 
 def banish_gems(start_idx):
@@ -271,7 +299,7 @@ def banish_gems(start_idx):
             break
         gems[i] = 5
     print_gems()
-
+    return gem
 def shift_gems():
     print_gems()
     for i in range(len(gems)-1,-1,-1):
@@ -282,12 +310,30 @@ def shift_gems():
     spawn_gems()
 
 def spawn_gems():
-    global gems
     for i in range(len(gems)):
         if gems[i] == 5:
             gems[i] = random.randint(0,4)
     print_gems()
     print()
 
-#メイン関数の呼び出し
+def element_boost(friend,monster):
+    eles='火風土水'
+    friend_ele=eles.index(friend['element'])
+    monster_ele=eles.index(monster['element'])
+    boost_values=[1.0,0.5,1.0 ,2.0]
+    boost_idx = (friend_ele+4 -monster_ele) % 4
+    return boost_values[boost_idx]
+
+def blur_damage(damage):
+    rand = random.uniform(-0.1,0.1)+1
+    return int(damage*rand)
+
+def do_recover(party,combo):
+    recover_value=min(20 * combo,party['max_hp']-party['hp'])
+    party['hp'] += recover_value
+    print(f'HPが{recover_value}回復した！(HP = {party['hp']})')
+    
+
+# main関数の呼び出し
 main()
+
